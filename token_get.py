5215706852ddb92961157db0d498eb24202ce6bd
Jeremy Domasian <j.domasian@gmail.com>
2023-11-17 15:13:46 -0800

    hardcode redirect URI

--- token_get.py
+++ token_get.py
@@ -4,7 +4,7 @@ import os
 import webbrowser
 
 client_id = os.environ.get('ZOOM_CLIENT_ID')
-redirect_uri = os.environ.get('ZOOM_REDIRECT_URI')
+redirect_uri = 'http://localhost:8080/oauth/redirect' 
 
 auth_url = f'https://zoom.us/oauth/authorize?response_type=code&client_id={client_id}&redirect_uri={redirect_uri}'
 
